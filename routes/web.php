61ddf8829b1c06e01dd1ab16ba001759d832314c
AyushLochan <ayushlochan4u@gmail.com>
2023-12-18 17:43:45 +0530

    Project intial step

--- routes/web.php
+++ routes/web.php
@@ -17,25 +17,5 @@ use App\Http\Controllers\IdeaController;
 */
 
 Route::get('/', function() {
-    return view('./dashboard');
+    return view('./welcome');
 });
-
-Route::get('/terms', function() {
-    return view('./terms');
-});
-
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
-Route::post('/ideas', [IdeaController::class, 'store'])->name('ideas.store');
-
-
-require __DIR__.'/auth.php';
